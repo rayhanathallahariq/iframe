ec672965f8398485ac1e25efef0bc84f9e78a0a8
rayhanathallahariq <108610498+rayhanathallahariq@users.noreply.github.com>
2024-06-19 10:42:25 +0700

    Add files via upload

---
+++ client.js
@@ -0,0 +1,20 @@
+const WebSocket = require("ws");
+
+const client = new WebSocket("ws://localhost:8081");
+
+//handling the event when the connection to server succsess
+client.on("open", ()=>{
+    console.log("connected");
+
+    client.on("close", (code)=>{
+        console.log(`Connection Lost with Code : ${code}`);
+    })
+})
+
+client.on("message",(message)=>{
+    console.log(`${message}`);
+})
+
+client.on("error", (error)=>{
+    console.log(`ERROR : ${error.message}`);
+})
