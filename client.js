1d6ca1c917adaadb8ac816a632a975794ca3d92d
rayhanathallahariq <rayhanariq59@gmail.com>
2024-06-19 15:13:10 +0700

    client

--- client.js
+++ client.js
@@ -1,10 +1,6 @@
 const WebSocket = require("ws");
 
-const client = new WebSocket("ws://localhost:8081",{
-    headers : {
-        'Authorization': '123456789'
-      }
-});
+const client = new WebSocket("ws://localhost:8081");
 
 //handling the event when the connection to server succsess
 client.on("open", ()=>{
